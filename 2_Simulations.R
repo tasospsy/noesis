3f1da3501c33528e99a66fd52cf939bf3faec4a5
Tasos Psychogyiopoulos <78209101+tasospsy@users.noreply.github.com>
2021-03-17 17:43:23 +0100

    Add files via upload

---
+++ 2_Simulations.R
@@ -0,0 +1,51 @@
+## Internship project
+## Tasos Psychogyiopoulos
+## SIMULATIONS
+## c.15/3/2021
+
+# ---- required sources
+source(url("https://raw.githubusercontent.com/tasospsy/noesis/main/Background_Functions.R"))
+source(url("https://raw.githubusercontent.com/tasospsy/noesis/main/1_Preparation.R"))
+
+## -- Data generation procedure
+set.seed(1992) # Set a seed
+Rawdata <- replicate(n = 1000,                            # 1000 replications
+                     lapply(lapply(modslist, GetCorMat),  # Get sigmas inside 
+                            function(s){
+                              mvrnorm(n = WAIS$size$GER,  # N = 1425 
+                                      mu = rep(0, nobs),  # nobs = 15
+                                      Sigma = s,
+                                      empirical = FALSE)
+                            }
+                     ),
+                     simplify = FALSE)
+
+# save(Rawdata, file = "Rawdata.Rda")
+
+# Here: ...? Download 'Rawdata' from github
+## -- Simulations
+# Let's specify the arguments to combine later with mapply function
+args <-  list(fitHF = c(HFmodel = T, BFmodel = F, NWmodel = F),
+              fitBF = c(HFmodel = F, BFmodel = T, NWmodel = F),
+              fitNW = c(HFmodel = F, BFmodel = F, NWmodel = T))
+
+# Perform the simulations
+startt <- Sys.time()
+out <- lapply(Rawdata, lapply, function(d) mapply(ModelsFitFun,
+                                                  args$fitHF,
+                                                  args$fitBF,
+                                                  args$fitNW,
+                                                  MoreArgs = list(Raw = T,
+                                                                  dat = d),
+                                                  SIMPLIFY = FALSE))
+endt <- Sys.time()
+endt-startt
+#  save(out, file = "out.Rda")
+
+## Here: ... New package to upload big files in github.
+
+# NOTE:
+# Time difference of 53.93103 mins
+# We can use 'mcmapply' and 'mclapply' instead of 'mapply' and 'lapply'
+# for parallel computing using multiple cores at once. 
+
